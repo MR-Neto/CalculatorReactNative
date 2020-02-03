73302f522de0394b4b19f0724cd05c680a4d80a0
Miguel Neto <mneto@niikiis.com>
2020-02-03 20:00:19 -0300

    Add flex to ButtonCalculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,16 +4,19 @@ import { ButtonText } from './Text'
 import styled from 'styled-components/native'
 import ColorConfig from '../configs/ColorConfig'
 
-export const ButtonCalculator = ({ children, onPress, isOperation }) => {
+export const ButtonCalculator = ({ children, onPress, isOperation, flex }) => {
     return (
-        <ButtonContainer onPress={onPress} isOperation={isOperation} >
+        <ButtonContainer onPress={onPress} isOperation={isOperation} flex={flex} >
             <ButtonText>{children}</ButtonText>
         </ButtonContainer>
     )
 }
 
 const ButtonContainer = styled(TouchableOpacity)`
+    border: 1px white;
+    height: 100%;
     justify-content: center;
     align-items: center;
-    background-color: ${({ isOperation }) => isOperation ? ColorConfig.orange : ColorConfig.lightGrey};
+    flex: ${({ flex }) => flex || 1};
+    background-color: ${ ({ isOperation }) => isOperation ? ColorConfig.orange : ColorConfig.lightGrey };
 `
