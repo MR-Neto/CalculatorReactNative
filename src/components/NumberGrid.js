90d70adce7988be882b923ec4e4a575482cdff1d
Miguel Neto <mneto@niikiis.com>
2020-02-03 18:56:36 -0300

    Created NumberGrid

---
+++ src/components/NumberGrid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const NumberGrid = () => {
+    return (
+        <View>
+            <Text />
+        </View>
+    )
+}
+
+export default NumberGrid
