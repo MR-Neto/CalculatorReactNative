08ddbd00f5769bb4be79ab4b1646993b1f391fe2
Miguel Neto <mneto@niikiis.com>
2020-02-03 19:24:35 -0300

    Created Text

---
+++ src/components/Text.js
@@ -0,0 +1,19 @@
+import { Text } from 'react-native'
+
+import styled from 'styled-components/native'
+import { SanFrancisco } from '../configs/FontConfig'
+import ColorConfig from '../configs/ColorConfig'
+
+const TextComponentStyled = styled(Text)`
+    font-size: 20px;
+    font-weight: 500;
+    font-family: ${SanFrancisco};
+`
+
+export default TextComponentStyled
+
+export const ButtonText = styled(TextComponentStyled)`
+    color: ${ColorConfig.white};
+    text-align: center;
+`
+
