ca397c5f791ada878130f9e57c46a50dea9c048d
Miguel Neto <mneto@niikiis.com>
2020-02-03 19:33:34 -0300

    Implemented ButtonCalculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import { ButtonText } from './Text'
+import styled from 'styled-components/native'
+import ColorConfig from '../configs/ColorConfig'
 
-const Button = () => {
+export const ButtonCalculator = ({ children, onPress, isOperation }) => {
     return (
-        <View>
-            <Text />
-        </View>
+        <ButtonContainer onPress={onPress} isOperation={isOperation} >
+            <ButtonText>{children}</ButtonText>
+        </ButtonContainer>
     )
 }
 
-export default Button
+const ButtonContainer = styled(TouchableOpacity)`
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ isOperation }) => isOperation ? ColorConfig.orange : ColorConfig.lightGrey};
+`
