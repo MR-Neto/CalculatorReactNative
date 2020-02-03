57e17fc1be8816e8a1c6702e50ba5fc996f7b056
Miguel Neto <mneto@niikiis.com>
2020-02-03 20:00:55 -0300

    Created Viewjs

---
+++ src/components/View.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+import styled from 'styled-components/native'
+
+export const Row = styled(View)`
+    flex:1;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items:center;
+`
