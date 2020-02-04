9bb118120c20791e1461e5297c7b5651986673b6
Miguel Neto <mneto@niikiis.com>
2020-02-03 23:54:07 -0300

    Renamed multiplication to x

--- src/helpers/calculatorHelper.js
+++ src/helpers/calculatorHelper.js
@@ -2,7 +2,7 @@ export const calculate = ({ previousNumber, number, operation }) => {
     switch (operation) {
         case '/':
             return Math.round(String(Number(previousNumber) / Number(number)) * 10000) / 10000
-        case 'X':
+        case 'x':
             return Math.round(String(Number(previousNumber) * Number(number)) * 10000) / 10000
         case '-':
             return Math.round(String(Number(previousNumber) - Number(number)) * 10000) / 10000
